779c8246c23f8ea215642eaf88b91d9ac1d18c0e
Syed Muhammad Taha <syedtaha456774@gmail.com>
2025-08-11 00:21:17 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,95 @@
+//Q1
+// var a = prompt("What is your name?")
+// alert(a);
+// console.log(a);
+
+//Q2
+// var b = prompt("Enter a Number");
+// if (b>10){
+//     console.log("Number is Big")
+// }
+// else{
+//     console.log("Number is Small")
+// }
+
+//Q3
+// var total = 50;
+// var b = total + 25;
+// alert(b)
+
+//Q4
+// var color = prompt("Enter your favourite color");
+// console.log("'Wow!" + color + "  is my favourite tool.'");
+
+//Q5
+// var age = prompt("What is your age?")
+// if(age>=18){
+//     console.log(" 'You are eligible to vote'.");
+// }
+
+//Q6
+// var a = prompt("Enter first number");
+// var b = prompt("Enter second number");
+// var c = a + b ;
+// alert(c);
+
+//Q7
+// var a = prompt("Enter Yes or No");
+// if(a == "Yes"){
+//     console.log("Great!");
+// }
+
+//Q8
+// var password = prompt("Enter your password");
+// if(password == 12345){
+//     console.log("Access granted"); 
+// }
+
+//Q9
+// var num1 = prompt("Enter first number");
+// var num2 = prompt("Enter second number");
+// if(num1 === num2){
+//     console.log("Both are equal");
+// }
+
+//Q10
+// var myName = "Syed Muhammad Taha";
+// alert(myName);
+
+//Q11
+// var temperature = prompt("What is the Temperature");
+// if(temperature > 30){
+//     console.log("It's hot today.");
+// }
+
+//Q12
+// var a = "Hello";
+// var b = " World";
+// var c = a + b;
+// alert(c)
+
+//Q13
+// var num = prompt("Enter number");
+// if(num / 2){
+//     console.log("Even Number");
+// }
+
+//Q14
+// var city = prompt("What is your city?");
+// if(city == "Karachi"){
+//     console.log("Welcome to the city of Lights"); 
+// }
+
+//Q15
+// var marks = prompt("Enter your marks.");
+// if(marks >= 50){
+//     console.log("Pass");
+// }
+// else{
+//     console.log("Fail");
+// }
+
+
+
+
+
